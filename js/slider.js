66f6b062fa0f03bb1db8a1cbdadc93b9d1283aaf
yamakamisaki <s.chuuu02@gmail.com>
2022-07-06 17:45:56 +0900

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.sliders').slick({
+      dots: true, //下の丸ぽっち（インジケーター）を表示 
+      autoplay: true, //自動再生 
+      autoplaySpeed: 5000 //自動再生の速度 
+      //ここにオプションを書いていく 
+  });
+});
